c019b96a14b7800e129d181ef10c1c7bf0c55b3e
HtunAungKyaw73 <htunaungkyaw730@gmail.com>
2025-09-04 16:17:59 +0700

    Layout Fixed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,9 +6,9 @@ import ActivePie from "@/components/active-pie";
 
 export default function Home() {
   return (
-    <div className="font-sans justify-items-center p-8">
-       <header className="mb-8 justify-items-center">
-           <div className="mb-8 justify-items-center">
+    <div className="font-sans p-8">
+       <header className="mb-8">
+           <div className="mb-8 flex flex-col items-center">
                <h1 className="text-xl md:text-2xl font-bold text-gray-800 text-center leading-relaxed ">Myanmar’s Key Stakeholders and their Perceptions <br/> of Sino-Myanmar Relations – A Survey (2024)</h1>
                <a className="text-xs md:text-sm text-center block font-bold cursor-pointer text-white mt-4 p-2 bg-red-500 w-1/3 rounded-md hover:bg-red-600" href="#main">5 Key Findings</a>
            </div>
@@ -17,8 +17,8 @@ export default function Home() {
                <p><i>Photo: AFP</i></p>
            </div>
        </header>
-      <main id="main" className={"md:w-3/4 lg:w-2/4"}>
-        <div>
+      <main id="main" className={"flex flex-col items-center"}>
+        <div className={"md:w-3/4 lg:w-2/4"}>
             <PieChartApp payload={data[3]}/>
             <PieChartApp payload={data[2]}/>
             <BarChartApp payload={data[0]}/>
@@ -26,7 +26,7 @@ export default function Home() {
             <BarChartApp payload={data[1]}/>
         </div>
       </main>
-      <footer className="justify-center mt-5 text-sm">
+      <footer className="flex justify-center mt-5 text-sm">
         <p>China Survey 2024 | ISP-Myanmar</p>
       </footer>
     </div>
